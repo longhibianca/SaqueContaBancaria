6e68d058494b1e87a1c191c22bf4fae02a58019c
Bianca Longhi <longhisbianca@gmail.com>
2021-11-18 13:10:12 -0300

    criando projeto para saque em conta utilizando exceptions

---
+++ SaqueContaBancaria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SaqueContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
