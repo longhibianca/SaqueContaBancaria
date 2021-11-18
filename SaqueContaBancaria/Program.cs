60e013346068554b9866df439e96f0e952de1a21
Bianca Longhi <longhisbianca@gmail.com>
2021-11-18 13:45:16 -0300

    criada logica do programa principal

--- SaqueContaBancaria/Program.cs
+++ SaqueContaBancaria/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SaqueContaBancaria.Entities;
+using SaqueContaBancaria.Entities.Exceptions;
+using System;
+using System.Globalization;
 
 namespace SaqueContaBancaria
 {
@@ -6,7 +9,32 @@ namespace SaqueContaBancaria
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial Balance: ");
+            double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw Limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, initialBalance, withdrawLimit);
+            Console.WriteLine(" ");
+            Console.Write("Enter amount for withdraw: ");
+            double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                account.WithDraw(withdraw);
+                Console.WriteLine("New Balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            
         }
     }
 }
